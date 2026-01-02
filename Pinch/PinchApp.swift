b6de2fd8304b1d1daa9de1ef6313bd3c30aa6fcb
Preeteesh Remalli <priteeshh@gmail.com>
2026-01-03 01:05:56 +0400

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Preeteesh Remalli on 03/01/26.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
